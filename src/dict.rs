bb4600d4e58a78ad222dbff4d69214574900a2c2
Mikhail Ivanov <m.ivanych@gmail.com>
2025-01-10 00:09:50 +0300

    Правила переделаны на более гибкую трёхкомпонентную форму

--- src/dict.rs
+++ src/dict.rs
@@ -1,17 +1,18 @@
 pub fn filter(words: Vec<String>) -> Vec<String> {
     // Правила
     let rules = [
-        (String::from("п"), String::from("1")),
-        (String::from("а"), String::from("*")),
-        (String::from("А"), String::from("2")),
-        (String::from("С"), String::from("*")),
-        (String::from("Т"), String::from("*")),
-        (String::from("А"), String::from("5")),
-        (String::from("о"), String::from("2")),
-        (String::from("В"), String::from("*")),
-        (String::from("а"), String::from("4")),
-        (String::from("Р"), String::from("*")),
-
+        (String::from("п"), String::from("+"), String::from("1")),
+        (String::from("а"), String::from("!"), String::from("*")),
+        (String::from("с"), String::from("+"), String::from("*")),
+        (String::from("с"), String::from("!"), String::from("3")),
+        (String::from("т"), String::from("!"), String::from("*")),
+        (String::from("е"), String::from("!"), String::from("*")),
+        (String::from("н"), String::from("!"), String::from("*")),
+        (String::from("я"), String::from("!"), String::from("*")),
+        (String::from("о"), String::from("+"), String::from("2")),
+        (String::from("и"), String::from("+"), String::from("+")),
+        (String::from("и"), String::from("!"), String::from("3")),
+        (String::from("с"), String::from("!"), String::from("4")),
     ];
     dbg!(&rules);
 
@@ -19,27 +20,30 @@ pub fn filter(words: Vec<String>) -> Vec<String> {
 
     let ass = dict_iter
         .clone()
-        .filter(|w| {
+        .filter(|word| {
             //dbg!(&w);
 
             // Длина
-            if w.len() != 10 {
+            if word.len() != 10 {
                 return false;
             }
 
             rules.iter().all(|rule| {
                 //dbg!(rule);
 
-                let (letter, position) = rule;
+                let (letter, condition, position) = rule;
                 let position = position.parse::<usize>().ok();
-                dbg!(letter, position);
+                dbg!(letter, condition, position);
+
+                let letter_lc = letter.to_string().as_str().to_lowercase();
+
+                let letter_pattern = letter_lc.as_str();
 
                 // любое место
                 if position.is_none() {
-                    let is_contain =
-                        w.contains(letter.to_string().as_str().to_lowercase().as_str());
+                    let is_contain = word.contains(letter_pattern);
 
-                    if letter.chars().all(char::is_lowercase) {
+                    if condition.to_string() == "+".to_string() {
                         is_contain
                     } else {
                         !is_contain
@@ -47,17 +51,12 @@ pub fn filter(words: Vec<String>) -> Vec<String> {
                 }
                 //  указанное место
                 else if position.is_some() {
-                    let end = position.unwrap() * 2;
-                    let start = end - 2;
-                    //dbg!(start, end);
-
-                    let symbol = w.get(start..end);
+                    let symbol = symbol(word, position);
                     //dbg!(symbol);
 
-                    let is_match =
-                        symbol == Some(letter.to_string().as_str().to_lowercase().as_str());
+                    let is_match = symbol == letter_pattern;
 
-                    if letter.chars().all(char::is_lowercase) {
+                    if condition.to_string() == "+".to_string() {
                         is_match
                     } else {
                         !is_match
@@ -72,4 +71,14 @@ pub fn filter(words: Vec<String>) -> Vec<String> {
         .collect::<Vec<_>>();
 
     ass
+}
+
+fn symbol(word: &String, position: Option<usize>) -> &str {
+    let end = position.unwrap() * 2;
+    let start = end - 2;
+    //dbg!(start, end);
+
+    let symbol = word.get(start..end).unwrap();
+
+    symbol
 }
