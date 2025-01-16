a71699ed4a4bc83ad34c6b5eccf30b0681ef8e25
Mikhail Ivanov <m.ivanych@gmail.com>
2025-01-16 18:56:27 +0300

    эксперимент с тестами, возвращающими Result вместо паники

--- src/args/tests.rs
+++ src/args/tests.rs
@@ -14,7 +14,25 @@ fn build_test_success() {
 fn build_test_fail() {
     let args = vec!["cmd".to_string()];
 
-    let args = Args::build(&args).unwrap();
+    Args::build(&args).unwrap();
+}
 
-    assert_eq!(args.file, "slovar.txt")
+#[test]
+fn build_test_ok() -> Result<(), String> {
+    let args = vec!["cmd".to_string(), "slovar.txt".to_string()];
+
+    let args = Args::build(&args)?;
+
+    assert_eq!(args.file, "slovar.txt");
+
+    Ok(())
+}
+
+#[test]
+fn build_test_err() -> Result<(), String> {
+    let args = vec!["cmd".to_string()];
+
+    assert!(Args::build(&args).is_err());
+
+    Ok(())
 }
