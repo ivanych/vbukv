2e14c59ec1d5cb3bc2d906311459569f834bfd21
Mikhail Ivanov <m.ivanych@gmail.com>
2025-03-03 17:38:32 +0300

    Логика больше не зависит от ввода

--- src/libvbukv/mod.rs
+++ src/libvbukv/mod.rs
@@ -4,24 +4,21 @@ pub mod rule;
 #[cfg(test)]
 mod tests;
 
-use crate::input::args::Args;
 use dict::Dict;
+use rule::Rule;
+use std::path::PathBuf;
 
-pub fn assumptions(args: &Args) -> Vec<String> {
+pub fn assumptions(file: &PathBuf, length: usize, rules: &Vec<Rule>) -> Vec<String> {
     // Прочитать слова из файла
-    let words = file::words_from_file(&args.file);
-    println!(
-        "Прочитан файл {} ({} слов)",
-        args.file.display(),
-        words.len()
-    );
+    let words = file::words_from_file(&file);
+    println!("Прочитан файл {} ({} слов)", file.display(), words.len());
 
     // Создать словарь
     let dict = Dict::new(words);
     //dbg!(&dict);
 
     // Найти предположения
-    let assumptions = dict.filter(args.length, &args.rules);
+    let assumptions = dict.filter(length, rules);
 
     assumptions
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let args = input::args::argsparse();
 
     // Найти предположения
-    let assumptions = libvbukv::assumptions(&args);
+    let assumptions = libvbukv::assumptions(&args.file, args.length, &args.rules);
 
     // Вывести предположения
     output::term::output(&assumptions).expect("Failed to print output");
