71df6778f5b8c41779ed2f06a0718cdf1ee398cf
Mikhail Ivanov <m.ivanych@gmail.com>
2025-03-26 16:34:22 +0300

    Дописана документация модуля libvbukv

--- src/libvbukv/mod.rs
+++ src/libvbukv/mod.rs
@@ -1,4 +1,43 @@
 //! Поиск слов в словаре по буквам
+//!
+//! # SYNOPSIS
+//!
+//! ```
+//! use std::path::PathBuf;
+//! use std::str::FromStr;
+//! use vbukv::libvbukv;
+//!
+//! // словарь (этот файл есть в репозитории проекта)
+//! let file = PathBuf::from("test_slovar.txt");
+//!
+//! // длина искомых слов
+//! let length = 5;
+//!
+//! // список правил поиска
+//! let rules = vec![
+//!     libvbukv::Rule::from_str("с+").unwrap(),
+//!     libvbukv::Rule::from_str("о+3").unwrap(),
+//! ];
+//!
+//! // Найти слова, подходящие под заданные параметры
+//! let assumptions = libvbukv::assumptions(&file, length, &rules);
+//!
+//! // Проверка: это слово должно быть среди найденных
+//! let word = "стопа".to_string();
+//! assert!(assumptions.contains(&word));
+//! ```
+//!
+//! # DESCRIPTION
+//!
+//! Модуль libvbukv предлагает механизм для поиска слов в словаре по заданным
+//! правилам.
+//!
+//! В качестве словаря используется простой текстовый файл в формате
+//! "одна строка — одно слово".
+//!
+//! Ограничений на длину словаря нет. На практике автор использует словарь
+//! примерно из 150 тысяч слов (это практически все слова русского языка),
+//! в таком словаре модуль находит заданные слова примерно за 0.1 секунды.
 
 pub mod dict;
 pub mod file;
@@ -10,6 +49,43 @@ use dict::Dict;
 pub use rule::Rule;
 use std::path::PathBuf;
 
+/// Прочитать файл словаря и найти слова, подходящие под заданные параметры
+///
+/// Пример:
+///
+/// ```
+/// use std::path::PathBuf;
+/// use std::str::FromStr;
+/// use vbukv::libvbukv;
+///
+/// // словарь (этот файл есть в репозитории проекта)
+/// let file = PathBuf::from("test_slovar.txt");
+///
+/// // длина искомых слов
+/// let length = 5;
+///
+/// // список правил поиска
+/// let rules = vec![
+///     libvbukv::Rule::from_str("с+").unwrap(),
+///     libvbukv::Rule::from_str("о+3").unwrap(),
+/// ];
+///
+/// // Найти слова, подходящие под заданные параметры
+/// let assumptions = libvbukv::assumptions(&file, length, &rules);
+///
+/// // Проверка: это слово должно быть среди найденных
+/// let word = "стопа".to_string();
+/// assert!(assumptions.contains(&word));
+/// ```
+///
+/// Аргументы:
+///
+/// * `file` — относительный путь к файлу словаря.
+/// * `length` — длина искомых слов в буквах.
+/// * `rules` — список правил для поиска слов.
+///
+/// Синтаксис правил описан в модуле [rule].
+///
 pub fn assumptions(file: &PathBuf, length: usize, rules: &Vec<Rule>) -> Vec<String> {
     // Прочитать слова из файла
     let words = file::words_from_file(&file);
