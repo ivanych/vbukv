307bcadbf3d26fa6b776338fc6832627ccc0157e
Mikhail Ivanov <m.ivanych@gmail.com>
2025-04-10 17:57:07 +0300

    Обновление с мастера с разрешением конфликтов

--- src/input/args/tests.rs
+++ src/input/args/tests.rs
@@ -1,11 +1,20 @@
 use super::*;
 
 #[test]
-fn test_args_parse() {
-    let args = Args::parse_from(["vbukv,", "-l", "5", "-f", "test_slovar.txt", "c+"]);
+fn test_parse_from_normal() {
+    let args = parse_from(["vbukv,", "-l", "5", "-f", "test_slovar.txt", "c+"]);
 
     assert_eq!(args.length, 5);
     assert_eq!(args.file.into_os_string(), "test_slovar.txt");
     assert_eq!(args.rules.len(), 1);
     //assert_eq!(args.rules.iter().nth(0).unwrap().letter, 'c');
+}
+
+// TODO тут нужен тест parse_from с опцией --markdown-help,
+// но для этого нужен мок функции exit в parse_from
+#[test]
+fn test_validate_rule_position() {
+    let args = Args::parse_from(["vbukv,", "-l", "5", "-f", "test_slovar.txt", "c+6"]);
+
+    args.validate_rule_position();
 }
--- src/libvbukv/file.rs
+++ src/libvbukv/file.rs
@@ -1,3 +1,28 @@
+//! Чтение списка слов из файла
+//!
+//! # SYNOPSIS
+//!
+//! ```
+//! use vbukv::libvbukv::file;
+//!
+//! // словарь (этот файл есть в репозитории проекта)
+//! let filename = "test_slovar.txt".to_string();
+//!
+//! // Прочитать слова из файла
+//! let words = file::words_from_file(filename);
+//!
+//! // Проверка: должно быть прочитано 11 слов
+//! assert_eq!(words.len(), 11);
+//! ```
+//!
+//! # DESCRIPTION
+//!
+//! Модуль file предоставляет функции для чтения списка слов из файла.
+//!
+//! Файл должен быть простым текстовым файлом в формате
+//! "одна строка — одно слово". Если в строке будет больше одного слова,
+//! то это не вызовет технических проблем, но логика работы будет нарушена —
+//! вся строка будет считаться одним словом.
 #[cfg(test)]
 mod tests;
 
@@ -7,6 +32,24 @@ use std::{
     path::Path,
 };
 
+/// Прочитать список слов из файла
+///
+/// ```
+/// use vbukv::libvbukv::file;
+///
+/// // словарь (этот файл есть в репозитории проекта)
+/// let filename = "test_slovar.txt".to_string();
+///
+/// // Прочитать слова из файла
+/// let words = file::words_from_file(filename);
+///
+/// // Проверка: должно быть прочитано 11 слов
+/// assert_eq!(words.len(), 11);
+/// ```
+///
+/// Аргументы:
+///
+/// * `filename` — путь к файлу.
 pub fn words_from_file(filename: impl AsRef<Path>) -> Vec<String> {
     let file = File::open(filename).expect("no such file");
     let buf = BufReader::new(file);
